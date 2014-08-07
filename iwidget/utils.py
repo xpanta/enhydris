322fcfcf693149ef7943343102a2a69f4e07701a
Chris Pantazis <xpanta@gmail.com>
2014-08-07 16:20:25 +0300

    Dashboard modifications

--- iwidget/utils.py
+++ iwidget/utils.py
@@ -66,55 +66,147 @@ def statistics_on_daily(ts_daily, occupancy = 1):
     result['today'] = timeseries[today]*1000.0
     if math.isnan(result['today']): result['today'] = 0.0
     result['today_last_year'] = timeseries.get(
-            _dec_year(today), 0)*1000.0
+        _dec_year(today), 0)*1000.0
     if math.isnan(result['today_last_year']):
         result['today_last_year'] = 0.0
     last_7_days_timestamp = today-timedelta(days=6)
     result['last_7_days_timestamp'] = last_7_days_timestamp
     result['last_7_days'] = aggregate_period(
-            timeseries,
-            last_7_days_timestamp,
-            today)*1000.0
+        timeseries,
+        last_7_days_timestamp,
+        today)*1000.0
+    """
+        ADDED CURRENT WEEK CONSUMPTION VALUE BY CHRIS PANTAZIS
+    """
+    result['current_week'] = aggregate_period(
+        timeseries,
+        today-timedelta(days=today.weekday()),
+        today)*1000.0
+    """
+        ADDED LAST WEEK CONSUMPTION VALUE BY CHRIS PANTAZIS
+    """
+    result['last_week'] = aggregate_period(
+        timeseries,
+        today-timedelta(days=today.weekday()+7),
+        today-timedelta(days=today.weekday()+1))*1000.0
+    """
+        ADDED CURRENT WEEK LAST YEAR CONSUMPTION VALUE BY CHRIS PANTAZIS
+    """
+    result['current_week_last_year'] = aggregate_period(
+        timeseries,
+        _dec_year(today-timedelta(days=today.weekday())),
+        _dec_year(today))*1000.0
+    """
+        ADDED LAST MONTH CONSUMPTION VALUE BY CHRIS PANTAZIS
+    """
+    last_day_of_last_month = today.replace(day=1) - timedelta(days=1)
+    first_day_of_last_month = last_day_of_last_month.replace(day=1)
+    result['last_month'] = aggregate_period(
+        timeseries, first_day_of_last_month, last_day_of_last_month)*1000.0
+    """
+        CODE ADDED BY CHRIS PANTAZIS ENDS HERE
+    """
     result['last_7_days_last_year'] = aggregate_period(
-            timeseries,
-            _dec_year(today-timedelta(days=6)),
-            _dec_year(today))*1000.0
+        timeseries,
+        _dec_year(today-timedelta(days=6)),
+        _dec_year(today))*1000.0
     result['current_month'] = aggregate_period(
-            timeseries,
-            today.replace(day=1),
-            today)
+        timeseries,
+        today.replace(day=1),
+        today)
     result['current_month_last_year'] = aggregate_period(
-            timeseries,
-            _dec_year(today.replace(day=1)),
-            _dec_year(today))
+        timeseries,
+        _dec_year(today.replace(day=1)),
+        _dec_year(today))
     result['current_year'] = aggregate_period(
-            timeseries,
-            today.replace(day=1, month=1),
-            today)
+        timeseries,
+        today.replace(day=1, month=1),
+        today)
     result['last_year'] = aggregate_period(
-            timeseries,
-            _dec_year(today.replace(day=1, month=1)),
-            _dec_year(today))
+        timeseries,
+        _dec_year(today.replace(day=1, month=1)),
+        _dec_year(today))
     result['last_7_days_cost'] = monthly_cost_from_consumption(
-            result['last_7_days']*0.001)
+        result['last_7_days']*0.001)
     result['last_7_days_last_year_cost'] = monthly_cost_from_consumption(
-            result['last_7_days_last_year']*0.001)
+        result['last_7_days_last_year']*0.001)
     result['current_month_cost'] = monthly_cost_from_consumption(
-            result['current_month'])
+        result['current_month'])
     result['current_month_last_year_cost'] = monthly_cost_from_consumption(
-            result['current_month_last_year'])
+        result['current_month_last_year'])
     result['current_year_cost'] = monthly_cost_from_consumption(
-            result['current_year'])
+        result['current_year'])
     result['last_year_cost'] = monthly_cost_from_consumption(
-            result['last_year'])
+        result['last_year'])
     daily_consumption = occupancy * TARGET_CONSUMPTION
     result['target_daily'] = daily_consumption * 1000.0
     result['target_weekly'] = daily_consumption * 7.0 * 1000.0
     result['target_month'] = daily_consumption * today.day
     result['target_year'] = daily_consumption * ((today - 
             today.replace(month=1, day=1)).days + 1)
+    """
+        CODE ADDED BY CHRIS PANTAZIS at 07/08/2014
+        NEXT FRAGMENT ADDS DICTIONARY ENTRIES FOR TARGET
+        CONSUMPTION (Lit/Person/Day) AND DECIDES HOW GOOD IT WAS
+        FOR EXAMPLE IF CONSUMPTION IS LESS THAN 80 lt/p/d THEN NO OF SMILES
+        WILL BE 5:
+        result['current_day_target'] = ['1', '2', '3', '4', '5']
+        SO WE CAN DO A FOR-LOOP IN THE TEMPLATE TO SHOW len() SMILES
+        (IF array is empty then show sad face, instead!)
+    """
+    result['today_lpd'] = get_lpd_arr(result['today'], occupancy, 'day', today)
+    result['current_week_lpd'] = get_lpd_arr(result['current_week'],
+                                             occupancy, 'day', today)
+    result['current_month_lpd'] = get_lpd_arr(result['current_month'],
+                                              occupancy, 'day', today)
+    result['current_year_lpd'] = get_lpd_arr(result['current_year'],
+                                             occupancy, 'day', today)
+    """
+        CHRIS PANTAZIS CODE ENDS HERE
+    """
     cache.set(cache_key, result, 600)
     return result
+
+
+def get_lpd_arr(val, occupants, period, today):
+    """
+    Author: Chris Pantazis (07/08/2014)
+    This function returns an array of literals depending on
+    the target consumption per person per day for the current household.
+    This is actually a workaround.
+    Needed in <<summary.html>> template file to print correct number of smilies
+
+    :param val: Total consumption for period (float)
+    :param occupants: Number of occupants (int)
+    :param period: day, week, month, year (string)
+    :param today: today's date. (Divide by weekday instead of 7 for example)
+    :return: array that has a len of 5,4,3,2,1 or 0 for number of smilies
+    """
+    lpd = 0
+    #first find current period
+    if period == 'day':
+        lpd = val / occupants
+    elif period == 'week':
+        lpd = val / (today.weekday() + 1) / occupants
+    elif period == 'month':
+        lpd = val / today.day / occupants
+    elif period == 'year':
+        lpd = val / today.timetuple().tmyday / occupants
+
+    if lpd <= 80:
+        return [1, 2, 3, 4, 5]
+    elif lpd <= 90:
+        return [1, 2, 3, 4]
+    elif lpd <= 105:
+        return [1, 2, 3]
+    elif lpd <= 110:
+        return [1, 2]
+    elif lpd <= 120:
+        return [1]
+    elif lpd <= 150:
+        return []
+
+
 
 # Target consumption in kWh per capita per day
 TARGET_ENERGY_CONSUMPTION = 5.0
--- uc_03_1/views.py
+++ uc_03_1/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.core.cache import cache
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -13,6 +13,10 @@ night_end = 6
 
 @login_required
 def compare(request, username):
+    # cache_key = 'uc0301__%d' % str(request.GET)
+    # cache_value = cache.get(cache_key)
+    # if cache_value is not None:
+    #     return cache_value
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
               "Sep", "Oct", "Nov", "Dec"]
     winter = [1, 2, 3, 4, 9, 10, 11, 12]
@@ -298,6 +302,7 @@ def compare(request, username):
         'summer_data': summer_data,
         'winter_data': winter_data,
     }
+    # cache.set(cache_key, result, 600)
     variables = RequestContext(request, data)
     return render_to_response("_charts_.html", variables)
 
